5b1e9dc61cde732c83ee156aa83995056ffc425f
dgstjohn <dgstjohn@gmail.com>
2021-10-15 23:21:14 -0400

    fixed small client queries error, added client mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -12,6 +12,35 @@ mutation login($email: String!, $password: String!) {
 }
 `;
 
+export const ADD_USER = gql`
+mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+        token
+        user {
+            _id
+            username
+        }
+    }
+}
+`;
+
+export const SAVE_BOOK = gql`
+mutation saveBook($input: SaveBookInput) {
+    saveBook(input: $input) {
+        _id
+        username
+        email
+        savedBooks {
+            bookId
+            authors
+            image
+            description
+            title
+            link
+    }
+}
+`;
+
 export const REMOVE_BOOK = gql`
 mutation removeBook($bookId: ID!) {
     removeBook(bookId: $bookId) {
